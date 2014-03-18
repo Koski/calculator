c5a33cfaa1bea142362c707665a110bd5d3ac06d
Karikoski Antti J <anttkari@da1-cs-bk107-34.helsinki.fi>
2014-03-18 17:05:07 +0200

    done

--- js/app.js
+++ js/app.js
@@ -1,57 +1,131 @@
 var Calculator = Ember.Application.create();
 
-Calculator.Operation = Ember.Object.extend(Flame.Validatable, {
-    validations: {
-        value: [Flame.Validator.Number]
-    }
+//contains all button functionality. Anonym function is run right away and returns the functions
+Calculator.buttonsController = Ember.Object.create(
+    (function(){
+        var operation
+        var value = 0;
+        var memory = 0;
 
-});
+        function doOperation() {
+            value = operation(value,this.get('input'));
+            this.set('input', value)
+        }
 
-Calculator.operationsController = Ember.Object.create({
-    value: 0,
-    memory: 0,
+        function setValueAndClearInput() {
+            value = this.get('input')
+            this.set('input', '')
+        }
 
-    calc: function(input) {
-        this.set('input', eval(getInput()))
-    },
+        return {
 
-    getInput: function() {
-        return this.get('input');
-    },
+            increment: function() {             
+                if (operation) {
+                    doOperation.call(this);
+                    return;
+                }
+                setValueAndClearInput.call(this);
+                operation = function(a,b) {
+                    return parseFloat(a)+parseFloat(b);
+                }
+            },
 
-    increment: function() {
-        this.value = eval(this.get('input'))
-        this.set('input', this.value)
-        //this.value += '+'
-        this.value += '+';
-        this.set('input', this.value)
-    },
+            subtract: function() {
+                if(operation) {
+                    doOperation.call(this);
+                    return;
+                }
+                setValueAndClearInput.call(this);
+                operation = function(a,b) {
+                    return parseFloat(a) - parseFloat(b);
+                }
+            },
 
-    clear : function() {
-        this.value = 0;
-        this.set('input', '');
-    },
-    numCatenation: function() {
-        if (this.value == 0) {
-            this.value = '7'
-        } else {
-           this.value += '7' 
-        }     
-        this.set('input',this.value)
-    },
-    result: function() {
-        this.set('input', eval(this.get('input')))
-    }
-    //console.log('kk' + input);
-});
+            multiply: function() {
+                if (operation) {
+                    doOperation.call(this);
+                    return;
+                }
+                setValueAndClearInput.call(this);
+                operation = function(a,b) {
+                    return parseFloat(a) * parseFloat(b);
+                }
+            },
 
+            divide: function() {
+                if(operation) {
+                    doOperation.call(this);
+                    return;
+                }
+                setValueAndClearInput.call(this);
+                operation = function(a, b) {
+                    return parseFloat(a) / parseFloat(b);
+                }
+            },
+
+            signChange: function() {
+                value = this.get('input') * -1
+                this.set('input', value)
+            },
+
+            dot: function() {
+                this.set('input', this.get('input') + '.')
+            },
+
+            clear: function() {
+                value = 0;
+                operation = undefined;
+                this.set('input', '');
+            },
+
+            numCatenation: function(num) {
+                if (!this.get('input')) {
+                    this.set('input', num)
+                    return;
+                }
+                if (operation && value == this.get('input')) {
+                    this.set('input', num)
+                    return;
+                }
+                this.set('input', this.get('input')+''+num)
+            },
+
+            result: function() {
+                value = operation(value, this.get('input'))
+                operation = undefined;
+                this.set('input', value)
+            },
+
+            memClear: function() {
+                memory = 0;
+                this.set('input', '')
+            },
+
+            memMinus: function() {
+                memory -= parseFloat(this.get('input'))
+                this.set('input', '')
+            },
+
+            memPlus: function() {
+                memory += parseFloat(this.get('input'))
+                this.set('input','')
+            },
+
+            memRecall: function() {
+                this.set('input', memory)
+            }
+        }
+    })
+());
+
+//View with flame widgets. buttons are in the same order as they appear in the calculator (left to right)
 Calculator.RootView = Flame.RootView.extend({
 
-    controller: Calculator.operationsController,
+    controller: Calculator.buttonsController,
 
     childViews: ['input',
                  'memPlus', 'memClear', 'memMinus', 'memRecall',
-                 'clear', 'plusMinus', 'division', 'multiply',
+                 'clear', 'signChange', 'division', 'multiply',
                  'seven', 'eight', 'nine', 'subtract',
                  'four', 'five', 'six', 'increment',
                  'one', 'two', 'three', 'result',
@@ -64,22 +138,30 @@ Calculator.RootView = Flame.RootView.extend({
 
     memClear: Flame.ButtonView.extend({
         layout: {left: 1, top: 45, width: 30},
-        title: 'MC'
+        title: 'MC',
+        targetBinding: '^controller',
+        action: 'memClear'
     }),
 
     memPlus: Flame.ButtonView.extend({
         layout: {left: 32, top: 45, width: 30},
-        title: 'M+'
+        title: 'M+',
+        targetBinding: '^controller',
+        action: 'memPlus'
     }),
 
     memMinus: Flame.ButtonView.extend({
         layout: {left: 63, top: 45, width: 30},
-        title: 'M-'
+        title: 'M-',
+        targetBinding: '^controller',
+        action: 'memMinus'
     }),
 
     memRecall: Flame.ButtonView.extend({
         layout: {left: 94, top: 45, width: 30},
-        title: 'MR'
+        title: 'MR',
+        targetBinding: '^controller',
+        action: 'memRecall'
     }),
 
     clear: Flame.ButtonView.extend({
@@ -89,56 +171,86 @@ Calculator.RootView = Flame.RootView.extend({
         action: 'clear'
     }),
 
-    plusMinus: Flame.ButtonView.extend({
+    signChange: Flame.ButtonView.extend({
         layout: {left: 32, top: 70, width: 30},
-        title: '+/-'
+        title: '+/-',
+        targetBinding: '^controller',
+        action: 'signChange'
     }),
 
     division: Flame.ButtonView.extend({
         layout: {left: 63, top: 70, width: 30},
-        title: '÷'
+        title: '÷',
+        targetBinding: '^controller',
+        action: 'divide'
     }),
 
     multiply: Flame.ButtonView.extend({
         layout: {left: 94, top: 70, width: 30},
-        title: 'x'
+        title: 'x',
+        targetBinding: '^controller',
+        action: 'multiply'
     }),
 
     seven: Flame.ButtonView.extend({
         layout: {left: 1, top: 95, width: 30},
         title: '7',
-        targetBinding: '^controller',
-        action: 'numCatenation'
+        target: '^controller',
+        action: function() {
+            Calculator.buttonsController.numCatenation(7);
+        }
     }),
 
     eight: Flame.ButtonView.extend({
         layout: {left: 32, top: 95, width: 30},
-        title: '8'
+        title: '8',
+        target: '^controller',
+        action: function() {
+            Calculator.buttonsController.numCatenation(8);
+        }
     }),
 
     nine: Flame.ButtonView.extend({
         layout: {left: 63, top: 95, width: 30},
-        title: '9'
+        title: '9',
+        target: '^controller',
+        action: function() {
+            Calculator.buttonsController.numCatenation(9);
+        }
     }),
 
     subtract: Flame.ButtonView.extend({
         layout: {left: 94, top: 95, width: 30},
-        title: '-'
+        title: '-',
+        targetBinding: '^controller',
+        action: 'subtract'
     }),
 
     four: Flame.ButtonView.extend({
         layout: {left: 1, top: 120, width: 30},
-        title: '4'
+        title: '4',
+        target: '^controller',
+        action: function() {
+            Calculator.buttonsController.numCatenation(4);
+        }
     }),
 
     five: Flame.ButtonView.extend({
         layout: {left: 32, top: 120, width: 30},
-        title: '5'
+        title: '5',
+        target: '^controller',
+        action: function() {
+            Calculator.buttonsController.numCatenation(5);
+        }
     }),
 
     six: Flame.ButtonView.extend({
         layout: {left: 63, top: 120, width: 30},
-        title: '6'
+        title: '6',
+        target: '^controller',
+        action: function() {
+            Calculator.buttonsController.numCatenation(6);
+        }
     }),
 
     increment: Flame.ButtonView.extend({
@@ -150,17 +262,29 @@ Calculator.RootView = Flame.RootView.extend({
 
     one: Flame.ButtonView.extend({
         layout: {left: 1, top: 145, width: 30},
-        title: '1'
+        title: '1',
+        target: '^controller',
+        action: function() {
+            Calculator.buttonsController.numCatenation(1);
+        }
     }),
 
     two: Flame.ButtonView.extend({
         layout: {left: 32, top: 145, width: 30},
-        title: '2'
+        title: '2',
+        target: '^controller',
+        action: function() {
+            Calculator.buttonsController.numCatenation(2);
+        }
     }),
 
     three: Flame.ButtonView.extend({
         layout: {left: 63, top: 145, width: 30},
-        title: '3'
+        title: '3',
+        target: '^controller',
+        action: function() {
+            Calculator.buttonsController.numCatenation(3);
+        }
     }),
 
     result: Flame.ButtonView.extend({
@@ -172,14 +296,18 @@ Calculator.RootView = Flame.RootView.extend({
 
     zero: Flame.ButtonView.extend({
         layout: {left: 1, top: 170, width: 60},
-        title: '0'
+        title: '0',
+        target: '^controller',
+        action: function() {
+            Calculator.buttonsController.numCatenation(0);
+        }
     }),
 
     dot: Flame.ButtonView.extend({
         layout: {left: 63, top: 170, width: 30},
-        title: '.'
+        title: '.',
+        targetBinding: '^controller',
+        action: 'dot'
     })
-
-
     
 });
